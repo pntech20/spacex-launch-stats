4091f1bc3450e2511aa2379ac59dca76760b0dcc
pntech20 <pntech20@gmail.com>
2020-08-09 22:03:03 +0700

    Build

---
+++ public/precache-manifest.839320f2dbb8f7175567d1f6e508f4d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8b9781a39554c3412e81b0dab44b8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19a3fae1101e60a8cbec",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e7538dd0a963dd9db107",
+    "url": "/static/js/2.14596035.chunk.js"
+  },
+  {
+    "revision": "013708584ae97a744ec506df2bba30b2",
+    "url": "/static/js/2.14596035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a3fae1101e60a8cbec",
+    "url": "/static/js/main.25bc4e63.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
